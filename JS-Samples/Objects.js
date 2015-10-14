ae26e08ec441dfe8158ad98f7bc827a216f5f78a
endabarrett <Enda.Barrett@nuigalway.ie>
2015-10-14 14:34:02 +0100

    Objects.js
    Sample to demonstrate how to use Objects in JavaScript.

---
+++ JS-Samples/Objects.js
@@ -0,0 +1,18 @@
+var myObject = {};  // Creates an object
+
+
+// add properties to the object
+
+myObject.firstName = "Enda";
+myObject.surName = "Barrett";
+myObject.email = "barrettenda@gmail.com";
+myObject.age = 50;
+
+
+var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+
+// Assessing object properties
+
+console.log(person.age);
+
+console.log(person["firstName"]);
