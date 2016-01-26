c16a58dd236f2a83cd0ff3c795c42c28b1f2040e
endabarrett <Enda.Barrett@nuigalway.ie>
2016-01-26 09:42:15 +0000

    Create table.js

---
+++ table/services/table.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mean.basic').factory('Basic', [ '$http',
+  function($http) {
+    return {
+      name: 'basic',    
+      parseData : function(url){
+          var data = $http.get(url); 		  
+          return data;
+        }
+    };
+  }
+]);
